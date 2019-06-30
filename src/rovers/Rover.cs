7996aaab302d3958a6808da487150f6ded011646
bilal-tragate <bilal@tragate.com>
2019-06-30 22:59:21 +0300

    position fixed

--- src/rovers/Rover.cs
+++ src/rovers/Rover.cs
@@ -1,29 +1,32 @@
 ﻿using System;
 using System.Linq;
 
-namespace rovers {
-    public class Rover : IRover {
+namespace rovers
+{
+    public class Rover : IRover
+    {
         /* * L = Left  * R = Right * M = Move*/
-        private readonly char[] _letters = { 'L', 'R', 'M' };
+        private readonly char[] _letters = {'L', 'R', 'M'};
 
         public int X;
         public int Y;
         public string Command { get; private set; }
-        public Direction Direction;
+        public Direction Direction { get; private set; }
         public Point CurrentPoint;
 
-        public Rover (int x, int y, Direction direction, string cmd) {
+        public Rover(int x, int y, Direction direction, string cmd){
             this.X = x;
             this.Y = y;
             this.Direction = direction;
-            this.SetCommand (cmd);
+            this.SetCommand(cmd);
         }
 
-        private void SetCommand (string cmd) {
-            if (cmd.Any (item => _letters.Contains (item))) {
+        private void SetCommand(string cmd){
+            if (cmd.Any(item => _letters.Contains(item))){
                 this.Command = cmd;
-            } else {
-                throw new Exception ("Invalid input");
+            }
+            else{
+                throw new Exception("Invalid input");
             }
         }
 
@@ -31,17 +34,17 @@ namespace rovers {
         ///  run commands concurrently
         /// </summary>
         /// <param name="index"></param>
-        public Rover RunCommand (int index) {
-            if (this.Command.Length > index) {
-                switch (Command[index]) {
+        public Rover RunCommand(int index){
+            if (this.Command.Length > index){
+                switch (Command[index]){
                     case 'L':
-                        TurnLeft ();
+                        TurnLeft();
                         break;
                     case 'R':
-                        TurnRight ();
+                        TurnRight();
                         break;
                     case 'M':
-                        Move ();
+                        Move();
                         break;
                 }
             }
@@ -49,48 +52,63 @@ namespace rovers {
             return this;
         }
 
-        private void TurnLeft () {
-            if (this.Direction == Direction.E)
-                this.Direction = Direction.N;
-            else if (this.Direction == Direction.N)
-                this.Direction = Direction.W;
-            else if (this.Direction == Direction.W)
-                this.Direction = Direction.S;
-            else if (this.Direction == Direction.S)
-                this.Direction = Direction.E;
+        private void TurnLeft(){
+            if (this.Direction == Direction.E) this.Direction = Direction.N;
+            else if (this.Direction == Direction.N) this.Direction = Direction.W;
+            else if (this.Direction == Direction.W) this.Direction = Direction.S;
+            else if (this.Direction == Direction.S) this.Direction = Direction.E;
         }
 
-        private void TurnRight () {
-            if (this.Direction == Direction.E)
-                this.Direction = Direction.S;
-            else if (this.Direction == Direction.S)
-                this.Direction = Direction.W;
-            else if (this.Direction == Direction.W)
-                this.Direction = Direction.N;
-            else if (this.Direction == Direction.N)
-                this.Direction = Direction.E;
+        private void TurnRight(){
+            if (this.Direction == Direction.E) this.Direction = Direction.S;
+            else if (this.Direction == Direction.S) this.Direction = Direction.W;
+            else if (this.Direction == Direction.W) this.Direction = Direction.N;
+            else if (this.Direction == Direction.N) this.Direction = Direction.E;
         }
 
         /// <summary>
         /// out of plateau exception
-        /// crus rover on same point 
+        /// crush rover on same point 
         /// </summary>
         /// <exception cref="NotImplementedException"></exception>
-        private void Move () {
-            if (this.Direction == Direction.E) {
-                if (this.CurrentPoint.Right == null) {
-                    throw new Exception ("Out of Plateau !");
-                } else if (this.CurrentPoint.Right.RoverOn != null) {
-                    throw new Exception ("Crush rovers !");
-                } else {
+        private void Move(){
+            switch (this.Direction){
+                case Direction.E:
+                    if (this.CurrentPoint.Right == null)
+                        throw new Exception("Out of plateau !");
+
                     this.CurrentPoint.RoverOn = null;
                     this.CurrentPoint.Right.RoverOn = this;
                     this.CurrentPoint = this.CurrentPoint.Right;
-                }
+                    break;
+                case Direction.W:
+                    if (this.CurrentPoint.Left == null)
+                        throw new Exception("Out of plateau !");
+
+                    this.CurrentPoint.RoverOn = null;
+                    this.CurrentPoint.Left.RoverOn = this;
+                    this.CurrentPoint = this.CurrentPoint.Left;
+                    break;
+                case Direction.N:
+                    if (this.CurrentPoint.Upper == null)
+                        throw new Exception("Out of plateau !");
+
+                    this.CurrentPoint.RoverOn = null;
+                    this.CurrentPoint.Upper.RoverOn = this;
+                    this.CurrentPoint = this.CurrentPoint.Upper;
+                    break;
+                case Direction.S:
+                    if (this.CurrentPoint.Bottom == null)
+                        throw new Exception("Out of plateau !");
+
+                    this.CurrentPoint.RoverOn = null;
+                    this.CurrentPoint.Bottom.RoverOn = this;
+                    this.CurrentPoint = this.CurrentPoint.Bottom;
+                    break;
             }
         }
 
-        public override string ToString () {
+        public override string ToString(){
             return $"{this.CurrentPoint.X} {this.CurrentPoint.Y} {this.Direction}";
         }
     }
